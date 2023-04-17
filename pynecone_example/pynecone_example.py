b9bb36288e0365a9b62ab6531cb42cf5f862fdb4
jjerry-k <jaeyeol2931@gmail.com>
2023-04-18 00:42:14 +0900

    Update

--- pynecone_example/pynecone_example.py
+++ pynecone_example/pynecone_example.py
@@ -1,42 +1,70 @@
-"""Welcome to Pynecone! This file outlines the steps to create a basic app."""
-from pcconfig import config
-
+"""Welcome to Pynecone! This file create a counter app."""
 import pynecone as pc
+from .state import State
 
-docs_url = "https://pynecone.io/docs/getting-started/introduction"
-filename = f"{config.app_name}/{config.app_name}.py"
-
-
-class State(pc.State):
-    """The app state."""
-
-    pass
-
-# Main page
-def index() -> pc.Component:
+def index():
+    """The main view."""
     return pc.center(
         pc.vstack(
-            pc.heading("Welcome to Pynecone!", font_size="2em"),
-            pc.box("Get started by editing ", pc.code(filename, font_size="1em")),
-            pc.link(
-                "Check out our docs!",
-                href=docs_url,
-                border="0.1em solid",
-                padding="0.5em",
-                border_radius="0.5em",
-                _hover={
-                    "color": "rgb(107,99,246)",
-                },
+            pc.hstack(
+                pc.circular_progress(
+                    pc.circular_progress_label("50", color="green"),
+                    value=50,
+                ),
+                pc.circular_progress(
+                    pc.circular_progress_label(
+                        "∞", color="rgb(107,99,246)"
+                    ),
+                    is_indeterminate=True,
+                ),
             ),
-            spacing="1.5em",
-            font_size="2em",
+            pc.heading(State.count),
+            pc.hstack(
+                pc.button("Decrement", on_click=State.decrement, color_scheme="red"),
+                pc.button(
+                    "Randomize",
+                    on_click=State.random,
+                    background_image="linear-gradient(90deg, rgba(255,0,0,1) 0%, rgba(0,176,34,1) 100%)",
+                    color="white",
+                ),
+                pc.button("Increment", on_click=State.increment, color_scheme="green"),
+            ),
+            padding="1em",
+            bg="#ededed",
+            border_radius="1em",
+            box_shadow="lg",
         ),
-        padding_top="10%",
+        padding_y="5em",
+        font_size="2em",
+        text_align="center",
     )
 
-
+def about():
+    return pc.center(
+        pc.vstack(
+            pc.avatar(
+                name="Jerry",
+                ),
+            pc.button(
+                "Fancy Button",
+                border_radius="1em",
+                box_shadow="rgba(151, 65, 252, 0.8) 0 15px 30px -10px",
+                background_image="linear-gradient(144deg,#AF40FF,#5B42F3 50%,#00DDEB)",
+                box_sizing="border-box",
+                color="white",
+                on_click=State.random,
+                _hover={
+                    "opacity": 0.85,
+                },
+            ),
+            pc.heading(State.count)
+        ),
+        padding_y="5em",
+        font_size="2em",
+        text_align="center",
+)
 # Add state and page to the app.
 app = pc.App(state=State)
-app.add_page(index)
-app.add_page(index)
+app.add_page(index, route="/")
+app.add_page(about, route="/about")
 app.compile()
---
+++ pynecone_example/state.py
@@ -0,0 +1,20 @@
+import pynecone as pc
+import random
+
+
+class State(pc.State):
+    """The app state."""
+
+    count = 0
+
+    def increment(self):
+        """Increment the count."""
+        self.count += 1
+
+    def decrement(self):
+        """Decrement the count."""
+        self.count -= 1
+
+    def random(self):
+        """Randomize the count."""
+        self.count = random.randint(0, 100)
